5ed5595125358ae2625eba3d62ac8531f13d437b
Vikas Patel <vikaspatel07071999@gmail.com>
2025-07-30 15:55:34 +0530

    dom

---
+++ Lect-17-js-dom/test.js
@@ -0,0 +1,26 @@
+        // console.log(document);
+        // console.log(document.head);
+        // console.log(document.body);
+        // document.body.style.color="red";
+// let ans=document.getElementsByTagName("h1"); 
+// console.log(ans);
+// ans[0].style.color="blue";
+// ans[1].style.backgroundColor="aqua";
+// ans[2].style.border="5px solid black";
+
+// for(let i=0;i<ans.length;i++){
+//     ans[i].style.color="black";
+// }
+
+let ans=document.getElementsByClassName("singer");
+console.log(ans);
+ans[0].innerHTML="sonu nigam";
+ans[1].style.color="green";
+
+
+
+// let ans=document.getElementById("rbu");
+// console.log(ans);
+// ans.style.backgroundColor="green";
+
+// ans.innerHTML="faizan sudhar jao";
