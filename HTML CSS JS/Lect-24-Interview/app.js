6ca811002dab99aa148197a62d111d63748ac951
Vikas Patel <vikaspatel07071999@gmail.com>
2025-08-13 15:55:53 +0530

    js and react02

---
+++ HTML CSS JS/Lect-24-Interview/app.js
@@ -0,0 +1,26 @@
+// let arr=[2,9,1,10,11,20,83,95];
+//   arr.slice(start, end);
+    // let ans= arr.slice(2,6);
+    // console.log(ans);
+    // console.log(arr);
+
+// let ans=arr.splice(2,4,100,200,999,45,23,20);
+// console.log(ans);
+// console.log(arr);
+
+// let arr=[2,9,1,10,11,20,83,95];
+// let key=100;
+// let ans=[500,...arr,key,200,300,];  //spread operator
+// console.log(ans);
+
+
+if(11==="11"){
+    console.log("hi");
+}
+else{
+    console.log("bye");
+    
+}
+
+    
+    
